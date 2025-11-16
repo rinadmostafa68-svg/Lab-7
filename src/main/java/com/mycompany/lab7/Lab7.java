bab120214110a97d46bdd4c575b2321332a4481f
RinadMostafa <es.renad.mostafa2023@alexu.edu.eg>
2025-11-16 12:16:14 +0200

    create

---
+++ src/main/java/com/mycompany/lab7/Lab7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab7;
+
+/**
+ *
+ * @author RinadMostafa
+ */
+public class Lab7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
